de5f7497b68a584fa869d005bb768c9ba8f984ee
Sarah Huang <shuang24@vols.utk.edu>
2024-03-26 17:19:04 -0400

    Init commit

---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress)){
+            if(canBePressed){
+                gameObject.SetActive(false);
+                //GameManager.instance.NoteHit();
+                if(Mathf.Abs(transform.position.y - 8) > 0.3){
+                    Debug.Log("normal hit");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+                else if(Mathf.Abs(transform.position.y - 8) > 0.15){
+                    Debug.Log("good hit");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else{
+                    Debug.Log("perfect hit");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Activator"){
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collilsion) {
+        if(collilsion.tag == "Activator" && gameObject.activeSelf){
+            canBePressed = false;
+            GameManager.instance.NoteMissed();
+            Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+        }
+    }
+}

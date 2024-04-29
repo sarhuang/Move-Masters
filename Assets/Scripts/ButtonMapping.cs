71fcedd526981b762ac7ec1c82b1377c082c5d56
Sarah Huang <shuang24@vols.utk.edu>
2024-04-29 16:00:23 -0400

    fix button mapping and add songs

--- Assets/Scripts/ButtonMapping.cs
+++ Assets/Scripts/ButtonMapping.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 //NOTE: If you need to remap any buttons, just change the KeyCode value
 public enum ButtonMap {
     BOTTOM_LEFT_ARROW = KeyCode.A,
+    TOP_LEFT_ARROW = KeyCode.F,
+    LEFT_ARROW = KeyCode.L,
+    DOWN_ARROW = KeyCode.D,
+    CENTER_ARROW = KeyCode.G,
+    UP_ARROW = KeyCode.J,
+    RIGHT_ARROW = KeyCode.H,
+    TOP_RIGHT_ARROW = KeyCode.S,
+    BOTTOM_RIGHT_ARROW = KeyCode.K
+}
+
+/*
+    BOTTOM_LEFT_ARROW = KeyCode.A,
     TOP_LEFT_ARROW = KeyCode.S,
     LEFT_ARROW = KeyCode.D,
     DOWN_ARROW = KeyCode.F,
@@ -13,4 +25,4 @@ public enum ButtonMap {
     RIGHT_ARROW = KeyCode.J,
     TOP_RIGHT_ARROW = KeyCode.K,
     BOTTOM_RIGHT_ARROW = KeyCode.L
-}
+*/
